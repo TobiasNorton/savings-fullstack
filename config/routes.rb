731f92b3dede4164a4eaf9d8b9ed275084236123
Tobias Norton <tnorton20@yahoo.com>
2019-03-13 15:26:46 -0400

    API scope added to goals resource,

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :goals
+  scope :api do
+    resources :goals
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Goal.create(name: 'Fender Telecaster', target_amount: 450000, balance: 60000, due_date: "2019-10-10")
+Goal.create(name: 'New Backpack', target_amount: 30000, balance: 15500, due_date: "2019-07-10")
+Goal.create(name: 'Morocco Trip', target_amount: 450000, balance: 60000, due_date: "2020-01-11")
+Goal.create(name: 'Ireland, Scotland Trip', target_amount: 450000, balance: 60050, due_date: "2020-05-20")
+Goal.create(name: 'Jeep Cherokee', target_amount: 450000, balance: 50000, due_date: "2019-08-15")
+Goal.create(name: 'New Glasses', target_amount: 20000, balance: 5000, due_date: "2019-04-15")
